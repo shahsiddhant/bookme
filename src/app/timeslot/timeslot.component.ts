55967c6f4eefd116271a1be1ba449a85f65d0133
siddhantshah-apx <siddhant_shah@comcast.com>
2017-10-13 16:54:12 -0400

    working shit

---
+++ src/app/timeslot/timeslot.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-timeslot',
+  templateUrl: './timeslot.component.html',
+  styleUrls: ['./timeslot.component.css']
+})
+export class TimeslotComponent implements OnInit {
+
+  constructor() { }
+  ngOnInit() {
+  }
+
+}
