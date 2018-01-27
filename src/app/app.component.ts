d11c77c7ca662389f24aed5ed0595413cdb1d85a
shahsiddhant <siddhant@siddhant.design>
2018-01-27 12:20:02 -0500

    updated package.json files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
 interface Week {
   day: string;
@@ -16,7 +17,7 @@ interface Rooms {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   weekDataCol: AngularFirestoreCollection<any>;
   document: AngularFirestoreDocument<any>;
@@ -24,7 +25,9 @@ export class AppComponent {
   roomDataCol: AngularFirestoreCollection<any>;
   roomData: Observable<any>;
   testdata: Observable<any>;
-  public data = [ '0915', '0930', '0945', '1000', '1015', '1030', '1045', '1100', '1115', '1130', '1145', '1200', '1215', '1230', '1245', '1300', '1315', '1330', '1345', '1400', '1415', '1430', '1445', '1500', '1515', '1530', '1545', '1600', '1615', '1630', '1645', '1700'];
+  public data = ['0915', '0930', '0945', '1000', '1015', '1030', '1045', '1100', '1115', '1130', '1145', '1200', '1215', '1230',
+    '1245', '1300', '1315', '1330', '1345', '1400', '1415', '1430', '1445',
+    '1500', '1515', '1530', '1545', '1600', '1615', '1630', '1645', '1700'];
   constructor(private afs: AngularFirestore) { }
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule }    from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { CalendarComponent } from './calendar/calendar.component';
@@ -9,28 +9,36 @@ import { DayComponent } from './day/day.component';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { SaveFormComponent } from './save-form/save-form.component';
+import { TimeAxisComponent } from './time-axis/time-axis.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDatepickerModule } from '@angular/material';
 
-var config = {
-  apiKey: "AIzaSyA8Ll1OmnuAzctmhfkxe3Xkze7lfaACAsQ",
-  authDomain: "bookme-11e75.firebaseapp.com",
-  databaseURL: "https://bookme-11e75.firebaseio.com",
-  projectId: "bookme-11e75",
-  storageBucket: "bookme-11e75.appspot.com",
-  messagingSenderId: "1007201102083"
-};
+// let config = {
+//   apiKey: 'AIzaSyA8Ll1OmnuAzctmhfkxe3Xkze7lfaACAsQ',
+//   authDomain: 'bookme-11e75.firebaseapp.com',
+//   databaseURL: 'https://bookme-11e75.firebaseio.com',
+//   projectId: 'bookme-11e75',
+//   storageBucket: 'bookme-11e75.appspot.com',
+//   messagingSenderId: '1007201102083'
+// };
 
 @NgModule({
   declarations: [
     AppComponent,
     CalendarComponent,
     DayComponent,
-    SaveFormComponent
+    SaveFormComponent,
+    TimeAxisComponent
+  ],
+  exports: [
+    MatDatepickerModule
   ],
   imports: [
     BrowserModule,
-    AngularFireModule.initializeApp(config),
     AngularFirestoreModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
